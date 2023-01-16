c8b47262c6a41de2fbfa8a822744ff34c1f3ad83
Noman Ejaz <nomanejaz132@gmail.com>
2023-01-16 23:25:23 +0500

    enteries()

---
+++ entries.js
@@ -0,0 +1,9 @@
+// The entries() method returns a new Array Iterator object that contains the key/value pairs
+// for each index in the array.
+
+// Example 1
+const array1 = ["a", "b", "c"];
+
+const iterator1 = array1.entries();
+
+console.log(iterator1.next().value);
