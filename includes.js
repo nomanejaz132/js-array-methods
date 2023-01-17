74252e147407490ef50ef83fb1f3cae47f6e2f0c
Noman Ejaz <nomanejaz132@gmail.com>
2023-01-17 22:44:57 +0500

    includes()

---
+++ includes.js
@@ -0,0 +1,12 @@
+// The includes() method determines whether an array includes a certain value among its entries,
+// returning true or false as appropriate.
+
+// Example 1
+const array1 = [1, 2, 3];
+
+console.log(array1.includes(2));
+
+const pets = ["cat", "dog", "bat"];
+
+console.log(pets.includes("cat"));
+console.log(pets.includes("at"));
