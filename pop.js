dda54f4cc6636dfd27646e769f7dfb9f3c67b9d0
Noman Ejaz <nomanejaz132@gmail.com>
2023-01-18 22:58:45 +0500

    pop()

---
+++ pop.js
@@ -0,0 +1,22 @@
+// The pop() method removes the last element from an array and returns that element.
+// This method changes the length of the array.
+
+// Example 1
+const plants = ["broccoli", "cauliflower", "cabbage", "kale", "tomato"];
+
+console.log(plants.pop());
+
+console.log(plants);
+
+plants.pop();
+
+console.log(plants);
+
+// Example 2
+const myFish = ["angel", "clown", "mandarin", "sturgeon"];
+
+const popped = myFish.pop();
+
+console.log(myFish);
+
+console.log(popped);
