8ffd082678b7c7245cd7a5d799279bbf0439920b
Noman Ejaz <nomanejaz132@gmail.com>
2023-01-18 23:34:51 +0500

    unshift()

---
+++ unshift.js
@@ -0,0 +1,9 @@
+// The unshift() method adds one or more elements to the beginning of an array and
+// returns the new length of the array.
+
+// Example 1
+const array1 = [1, 2, 3];
+
+console.log(array1.unshift(4, 5));
+
+console.log(array1);
